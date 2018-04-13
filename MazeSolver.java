29a7407ef76ff3c05eae18cf020a0b8d571ca769
Ray <rayonishi11@gmail.com>
2018-04-12 20:19:30 -0400

    Fixed issues with MazeSolver and added test cases in UserOfMazeSolver

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,38 +1,42 @@
 /**
-   Represent the set of steps needed for the solution(if any) of a Maze
- */
+Represent the set of steps needed for the solution(if any) of a Maze
+*/
 public class MazeSolver{
-    private Maze mazeHolder; /*holds the maze that will be solved*/
-    private int path;
-    public MazeSolver(Maze maze){
-	path=0;
-	mazeHolder=maze;
+  private Maze mazeHolder; /*holds the maze that will be solved*/
+  private int path;
+  public MazeSolver(Maze maze){
+    path=0;
+    mazeHolder=maze;
+  }
+  public boolean solve() {
+    if(mazeHolder.explorerIsOnA()==Maze.TREASURE){
+      path++;
+      return true;
     }
-    public boolean solver(Maze maze){
-	if(maze.explorerIsOnA()==Maze.TREASURE){
-	    path++;
-	    return true;
-	}
-	else if(maze.explorerIsOnA()==Maze.WALL){
-	    return false;
-	}
-	else{
-	for(int direction=1;direction<=8;direction*=2){
-	    Maze snapshot=new Maze(maze);
-	    maze.go(direction);
-	    System.out.println(maze);
-	    solver(maze);
-	    maze=snapshot;
-	}
-	if(path>0)return true;
-	return false;
-	}
+    else if(mazeHolder.explorerIsOnA()==Maze.WALL){
+      return false;
     }
-    /**
-      @return a string representation of this board
-     */
-    public String toString(){
-	if(path>0)return"There is a path for this maze.";
-	return "There is not  a path for this maze.";
+    else{
+      for(int direction=1;direction<=8;direction*=2){
+        Maze snapshot = new Maze(mazeHolder);
+        mazeHolder.dropA(Maze.WALL);
+        mazeHolder.go(direction);
+        solve();
+        mazeHolder=snapshot;
+      }
+      if(path>0) {
+        return true;
+      }
+      return false;
     }
+  }
+  /**
+  @return a string representation of this board
+  */
+  public String toString(){
+    if(path>0) {
+      return"There is a path for this maze.";
+    }
+    return "There is not a path for this maze.";
+  }
 }
--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,23 +1,40 @@
 /**
-   Tests MazeSolver.
-  Requires command line arguments:(maybe later)
-  o  the name of a file containing a maze.
-  o  the rank and file where an explorer is starting
+Tests MazeSolver.
+Requires command line arguments:(maybe later)
+o  the name of a file containing a maze.
+o  the rank and file where an explorer is starting
 
-  For example,
-      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
- */
+For example,
+java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+*/
 public class UserOfMazeSolver{
-    public static void main(String[] commandLine)
-       throws java.io.FileNotFoundException {
-        System.out.println();
+  public static void main(String[] commandLine)
+  throws java.io.FileNotFoundException {
+    System.out.println();
 
-        MazeSolver solver;
-	Maze mazeSample = new Maze( "mazes/intersection_treasureNorth.txt"
-                            ,1
-                            ,1
-                            );
-	solver=new MazeSolver(mazeSample);
-        System.out.println( solver + System.lineSeparator());
-    }
- }
+    // Expecting there to be a path
+    MazeSolver solver1;
+    Maze maze1 = new Maze( "mazes/intersection_treasureNorth.txt",1,1);
+    solver1 = new MazeSolver(maze1);
+    solver1.solve();
+    System.out.println( solver1 + System.lineSeparator() + maze1);
+
+    // Expecting there to be a path
+    Maze maze2 = new Maze( "mazes/4cell_treasureWest.txt",0,1);
+    MazeSolver solver2 = new MazeSolver(maze2);
+    solver2.solve();
+    System.out.println(solver2 + System.lineSeparator() + maze2);
+
+    // Expecting there to not be a path
+    Maze maze3 = new Maze("mazes/intersection_noTreasure.txt",1,1);
+    MazeSolver solver3 = new MazeSolver(maze3);
+    solver3.solve();
+    System.out.println(solver3 + System.lineSeparator() + maze3);
+
+    // Expecting there to be a path
+    Maze maze4 = new Maze("mazes/loop_withTreasure.txt",1,1);
+    MazeSolver solver4 = new MazeSolver(maze4);
+    solver4.solve();
+    System.out.println(solver4 + System.lineSeparator() + maze4);
+  }
+}
