abb6504e00dfb7997c3b4edd6213d1abe6f0a172
DevMMing <mming@stuy.edu>
2018-04-14 17:35:22 -0400

    added comments and added amount of paths to toString()

--- MazeSolver.java
+++ MazeSolver.java
@@ -2,21 +2,25 @@
 Represent the set of steps needed for the solution(if any) of a Maze
 */
 public class MazeSolver{
-  private Maze mazeHolder; /*holds the maze that will be solved*/
+    private Maze mazeHolder; //holds the maze that will be solved
   private int path;
+    /**
+      Construct an instance of a solver for a given @maze.
+     */
   public MazeSolver(Maze maze){
     path=0;
-    mazeHolder=maze;
+    mazeHolder=maze;//refer to the maze that needs to be solved
   }
   public boolean solve() {
     if(mazeHolder.explorerIsOnA()==Maze.TREASURE){
-      path++;
+	path++;//record successful path
       return true;
     }
     else if(mazeHolder.explorerIsOnA()==Maze.WALL){
       return false;
     }
-    else{
+    else{//recursive case
+	//for each direction in the path we're working on...
       for(int direction=1;direction<=8;direction*=2){
         Maze snapshot = new Maze(mazeHolder);
         mazeHolder.dropA(Maze.WALL);
@@ -31,12 +35,12 @@ public class MazeSolver{
     }
   }
   /**
-  @return a string representation of this board
+  @return a string representation of this maze
   */
   public String toString(){
     if(path>0) {
-      return"There is a path for this maze.";
+      return"There is "+ path+ " paths for this maze.";
     }
-    return "There is not a path for this maze.";
+    return "There are no  path for this maze.";
   }
 }
